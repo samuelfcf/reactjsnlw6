1e1923a8cab89e381596a4deff74e8a5c914f47e
Samuel Felipe <samuelfelipef@gmail.com>
2021-07-02 21:14:09 -0300

    add tela de auth, tela de criação de sala, roteamento e naveg e autenticação com firebase.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 
 function App() {
   return (
-      <h1>Hello Worldaaa</h1>
+      <BrowserRouter>
+        <Route path="/" component={Home} exact/>
+        <Route path="/rooms/new" component={NewRoom} />
+      </BrowserRouter>
   );
 }
 
